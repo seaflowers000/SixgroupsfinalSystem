5cc654de9042bd7f2af7a5d7d12a17f1663e6794
54454 <544548586@qq.com>
2024-12-30 18:15:16 +0800

    qwq

--- src/main/java/cn/lanqiao/sixgroupsfinalsystem/controller/ManagerController.java
+++ src/main/java/cn/lanqiao/sixgroupsfinalsystem/controller/ManagerController.java
@@ -1,4 +1,39 @@
 package cn.lanqiao.sixgroupsfinalsystem.controller;
 
+import cn.lanqiao.sixgroupsfinalsystem.mapper.ManagerMapper;
+import cn.lanqiao.sixgroupsfinalsystem.model.pojo.Manager;
+import cn.lanqiao.sixgroupsfinalsystem.model.pojo.VipName;
+import cn.lanqiao.sixgroupsfinalsystem.model.vo.MagVO;
+import cn.lanqiao.sixgroupsfinalsystem.service.ManagerService;
+import cn.lanqiao.sixgroupsfinalsystem.service.VipNameService;
+import cn.lanqiao.sixgroupsfinalsystem.service.impl.ManagerServiceImpl;
+import cn.lanqiao.sixgroupsfinalsystem.utils.ResponseUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/mang")
 public class ManagerController {
+    @Autowired
+    private ManagerServiceImpl managerService;
+    @GetMapping("/select")
+    public ResponseUtils select(){
+        try {
+            List<MagVO> magVOS = managerService.selectAll();
+            System.out.println("查询结果：" + magVOS);
+
+            if(magVOS != null && !magVOS.isEmpty()){
+                return new ResponseUtils(200, "查询成功", magVOS);
+            } else {
+                return new ResponseUtils(0, "暂无数据", new ArrayList<>());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseUtils(500, "查询失败: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/cn/lanqiao/sixgroupsfinalsystem/service/ManagerService.java
+++ src/main/java/cn/lanqiao/sixgroupsfinalsystem/service/ManagerService.java
@@ -1,4 +1,10 @@
 package cn.lanqiao.sixgroupsfinalsystem.service;
 
-public class ManagerService {
+import cn.lanqiao.sixgroupsfinalsystem.model.pojo.VipName;
+import cn.lanqiao.sixgroupsfinalsystem.model.vo.MagVO;
+
+import java.util.List;
+
+public interface ManagerService {
+    List<MagVO> selectAll();
 }
