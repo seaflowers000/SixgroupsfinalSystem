d2c31467d01b096511be4634f62a537bdc987a0b
YUE <2278674334@qq.com>
2025-01-06 14:39:46 +0800

    vipname

--- src/main/java/cn/lanqiao/sixgroupsfinalsystem/model/pojo/VipName.java
+++ src/main/java/cn/lanqiao/sixgroupsfinalsystem/model/pojo/VipName.java
@@ -1,8 +1,6 @@
 package cn.lanqiao.sixgroupsfinalsystem.model.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
@@ -10,13 +8,59 @@ import lombok.NoArgsConstructor;
 
 public class VipName {
 
-  private long id;
+  private Integer id;
+
   private String username;
   private String gender;
   private String email;
   private String address;
-  private long status;
+  private Integer status;
 
+  public Integer getId() {
+    return id;
+  }
 
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
 }
