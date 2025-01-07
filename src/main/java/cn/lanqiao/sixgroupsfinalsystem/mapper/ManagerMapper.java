34fcfda3b4b6c9303709df944eb7ee48eb0e2a89
54454 <544548586@qq.com>
2025-01-07 10:42:56 +0800

    模糊查询+删除

--- src/main/java/cn/lanqiao/sixgroupsfinalsystem/mapper/ManagerMapper.java
+++ src/main/java/cn/lanqiao/sixgroupsfinalsystem/mapper/ManagerMapper.java
@@ -1,10 +1,13 @@
 package cn.lanqiao.sixgroupsfinalsystem.mapper;
 
 import cn.lanqiao.sixgroupsfinalsystem.model.pojo.Manager;
+import cn.lanqiao.sixgroupsfinalsystem.model.pojo.VipName;
 import cn.lanqiao.sixgroupsfinalsystem.model.vo.MagVO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -12,4 +15,13 @@ import java.util.List;
 public interface ManagerMapper extends BaseMapper<Manager> {
     @Select("select * from manager")
     List<MagVO> selectAll();
+
+    @Update("UPDATE manager SET status = 1 WHERE id = #{id}")
+    int deleteById(Integer id);
+
+    @Update({
+            "<script>", "UPDATE manager SET status = 1", "WHERE id IN", "<foreach collection='list' item='item' open='(' separator=',' close=')'>", "#{item}", "</foreach>", "</script>"})
+    int batchDelete(List<Integer> list);
+    @Select("SELECT * FROM manager WHERE status = 0 AND login_name LIKE CONCAT('%', #{loginname}, '%')" )
+    List<Manager> likeselect(@Param("loginname") String loginname);
 }
