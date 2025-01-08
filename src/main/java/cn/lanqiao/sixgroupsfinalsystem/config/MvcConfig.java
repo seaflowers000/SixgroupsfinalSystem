68c8b0ffd35b70b7c643509c3c3c3a5b9abcd231
54454 <544548586@qq.com>
2025-01-08 11:49:25 +0800

    拦截器

--- src/main/java/cn/lanqiao/sixgroupsfinalsystem/config/MvcConfig.java
+++ src/main/java/cn/lanqiao/sixgroupsfinalsystem/config/MvcConfig.java
@@ -1,7 +1,29 @@
-package cn.lanqiao.sixgroupsfinalsystem.config;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class MvcConfig implements WebMvcConfigurer {
-    //拦截器
-}
+//package cn.lanqiao.sixgroupsfinalsystem.config;
+//
+//import cn.lanqiao.sixgroupsfinalsystem.interceptor.LoginInterceptor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer {
+//    @Autowired
+//    private LoginInterceptor loginInterceptor;
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                //放行
+//                .excludePathPatterns(
+//                        "/login.html",
+//                        "/css/**",
+//                        "/js/**",
+//                        "/fonts/**",
+//                        "/images/*",
+//                        "/*.ico",
+//                        "/Six",
+//                        "/codeImage"
+//                ).order(1);
+//    }
+//}
--- src/main/java/cn/lanqiao/sixgroupsfinalsystem/interceptor/LoginInterceptor.java
+++ src/main/java/cn/lanqiao/sixgroupsfinalsystem/interceptor/LoginInterceptor.java
@@ -1,5 +1,59 @@
-package cn.lanqiao.sixgroupsfinalsystem.interceptor;
-
-public class LoginInterceptor {
-    //拦截器
-}
+//package cn.lanqiao.sixgroupsfinalsystem.interceptor;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.thymeleaf.util.StringUtils;
+//
+//import java.util.concurrent.TimeUnit;
+//import static cn.lanqiao.sixgroupsfinalsystem.model.common.FinalClass.USER_TOKEN;
+//
+//
+//
+//@Component
+//@Slf4j
+//public class LoginInterceptor implements HandlerInterceptor {
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        String requestURI = request.getRequestURI();
+//        log.info("拦截所有:{}", requestURI);
+//        if (isStaticResource(requestURI)) {
+//            return true;
+//        }
+//        //先session拿到登录成功的session值
+//        String token=request.getHeader("token");
+//        if (StringUtils.isEmpty(token)){
+//            return false;
+//        }
+//        String redisKey=USER_TOKEN+token;
+//        String userInfo=stringRedisTemplate.opsForValue().get(redisKey);
+//        if (StringUtils.isEmpty(userInfo)){
+//            return false;
+//        }
+//        stringRedisTemplate.expire(redisKey,30L, TimeUnit.MINUTES);
+//        return true;
+//    }
+//    private boolean isStaticResource(String uri) {
+//        return uri.startsWith("/css/") ||
+//                uri.startsWith("/js/") ||
+//                uri.startsWith("/images/") ||
+//                uri.startsWith("/fonts/") ||
+//                uri.startsWith("/files/") ||
+//                uri.startsWith("/error") ||
+//                uri.startsWith("/pages") ||
+//                uri.startsWith("/lib/") ||
+//                uri.startsWith("/static/") ||
+//                uri.startsWith("/Six/") ||
+//                uri.startsWith("/mang/") ||
+//                uri.startsWith("/codeImage/") ||
+//                uri.endsWith(".html") ||
+//                uri.endsWith(".ico");
+//    }
+//}
